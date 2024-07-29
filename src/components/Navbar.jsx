d6caf5d7b0bb264c435b23884db2c8274bdc39cb
VIVEK KUMAR <vivkumar4798@gmail.com>
2024-07-29 14:29:24 +0530

    navbar.jsx updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => (
     sx ={{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
       <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
         <img src={logo} alt="logo" height={45} />
-        <Typography> VidStream</Typography>
+        <Typography variant='h5' fontWeight="bold" style={{color:'white'}} {...{marginLeft:'8px'}}> VidStream</Typography>
       </Link>
       <SearchBar/>
     </Stack>
